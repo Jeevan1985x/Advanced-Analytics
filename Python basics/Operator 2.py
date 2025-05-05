e22263760796628b033b800b09f89fd032c56bb0
Jeevan1985x <jeevankj2016@gmail.com>
2025-05-05 23:41:22 +1000

    Update Operator 2.py

--- Python basics/Operator 2.py
+++ Python basics/Operator 2.py
@@ -4,3 +4,10 @@ q = 'dog'
 
 # step 5
 print('mouse' not in z)
+
+
+#Create a variable z with the values 'cat' and 'dog' assigned to it, and a variable q with the value 'dog' (don’t forget to use square brackets)
+
+#Use an identity operator to determine that 'mouse' is not in the list assigned to z and then use the print() function to print your results
+
+#If you get stuck, refer to "main_solved.py" to help with coding in "main.py."
