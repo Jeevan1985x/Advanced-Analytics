c8b4760fd87f2c9595d17dcdd75c68ddcb67ede9
Jeevan1985x <jeevankj2016@gmail.com>
2025-05-05 23:48:42 +1000

    Update Variables.py

--- Python basics/Variables.py
+++ Python basics/Variables.py
@@ -1,3 +1,14 @@
+#Create two variables, a and b: assign the value of 10 to a and 20 to b 
+
+#Create a variable c and assign it the sum of a and b 
+
+#Print c 
+
+#Check if the sum of a and b is the same value as c using the comparison operator ==
+
+#Assign the result to compare_sums
+
+#If you get stuck, refer to "main_solved.py" to help with coding in "main.py."
 a, b = 10, 20
 
 c = a + b
