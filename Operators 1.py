9283839ed4c911ad10b3aaf05d7f9b35aa9e4255
Jeevan1985x <jeevankj2016@gmail.com>
2025-05-05 23:33:46 +1000

    Update Operators 1.py

--- Operators 1.py
+++ Operators 1.py
@@ -5,3 +5,11 @@ res=res-x
 print(res)
 res_2= y>x
 print(res_2)
+
+#Now it’s your turn. Perform the following tasks in the "main.py" notebook:
+
+#Create a variable x with the value 10 and a variable y with the value 20
+
+#Divide y by x and subtract x from this number to generate the output -8, then use the print() function to print your results
+
+#Use a comparison operator to determine whether y is greater than or equal to x, then use the print() function to print your results
